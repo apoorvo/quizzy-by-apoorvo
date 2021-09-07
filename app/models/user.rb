31d7684a08826b8d00e9f353116ae25b4bcf1a03
Apoorvo Chakraborty <apoorvo.chak@gmail.com>
2021-09-07 19:27:10 +0530

    Added password digest and has_secure_password to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
   enum role: { standard: 0, administrator: 1 }
 
+  has_secure_password
+
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name, presence: true, length: { maximum: 50 }
