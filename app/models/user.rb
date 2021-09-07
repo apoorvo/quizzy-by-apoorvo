8a266fb792ee1275ef07673d7626b92e6513f302
Apoorvo Chakraborty <apoorvo.chak@gmail.com>
2021-09-07 18:45:36 +0530

    Added role to user. Wrote tests for the same.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
+  enum role: { standard: 0, administrator: 1 }
 
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :first_name, presence: true, length: { maximum: 50 }
