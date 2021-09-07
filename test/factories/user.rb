d339efdff73455a1d11c635a566e4210da25b2af
Apoorvo Chakraborty <apoorvo.chak@gmail.com>
2021-09-08 04:20:04 +0530

    Added tests for password validation

--- test/factories/user.rb
+++ test/factories/user.rb
@@ -5,5 +5,7 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
+    password { "welcome" }
+    password_confirmation { "welcome" }
   end
 end
