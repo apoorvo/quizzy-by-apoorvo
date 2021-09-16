a47b37576044822d3977462d84371eccc643a075
Apoorvo Chakraborty <apoorvochakraborty@Apoorvos-MacBook-Pro.local>
2021-09-16 22:29:25 +0530

    Changed quiz schema

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -3,14 +3,5 @@
 class Quiz < ApplicationRecord
   belongs_to :user
 
-  validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
-  before_validation :set_name
-
-  private
-
-    def set_name
-      count = Quiz.where("name LIKE ?", "#{name}%").size
-      name_candidate = count.positive? ? "#{name} #{count + 1}" : name
-      self.name = name_candidate
-    end
+  validates :name, presence: true, length: { maximum: 50 }
 end
--- db/migrate/20210916100336_create_quizzes.rb
+++ db/migrate/20210916100336_create_quizzes.rb
@@ -3,7 +3,7 @@
 class CreateQuizzes < ActiveRecord::Migration[6.1]
   def change
     create_table :quizzes do |t|
-      t.string :name, null: false, index: { unique: true }
+      t.string :name, null: false
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end
