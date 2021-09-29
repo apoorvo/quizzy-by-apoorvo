a72cf8fcc750ec23db4649839c6370e0b03146f5
Apoorvo Chakraborty <apoorvo.chak@gmail.com>
2021-09-29 21:41:37 +0530

    Addtional changes to model associations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,6 @@ class SessionsController < ApplicationController
 
   def destroy
     reset_session
-
     render json: { status: 200, logged_out: true }
   end
 
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@
 
 class Question < ApplicationRecord
   belongs_to :quiz
-  has_many :attempt_answers
+  has_many :attempt_answers, dependent: :destroy
 
   validate :answer_is_valid
 
